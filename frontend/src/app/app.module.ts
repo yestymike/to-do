f632f4a6c6cdf2510fbd1061175473e4b2854ca9
Mike <27782678+yestymike@users.noreply.github.com>
2026-02-11 08:24:46 +0300

    Add app.module.ts for frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
@@ -11,7 +12,8 @@ import { TodoService } from './todo.service';
     TodoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [TodoService],
   bootstrap: [AppComponent]
