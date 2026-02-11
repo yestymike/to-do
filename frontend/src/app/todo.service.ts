75d03498c6957de119c067b0508bafacff7f9626
Mike <27782678+yestymike@users.noreply.github.com>
2026-02-11 08:24:56 +0300

    Add todo.service.ts for frontend

--- frontend/src/app/todo.service.ts
+++ frontend/src/app/todo.service.ts
@@ -1,33 +1,21 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Todo } from './todo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
   private todos: Todo[] = [];
-  private todosSubject = new BehaviorSubject<Todo[]>(this.todos);
-  todos$ = this.todosSubject.asObservable();
 
-  constructor() { }
-
-  getTodos(): Observable<Todo[]> {
-    return this.todos$;
+  getTodos(): Todo[] {
+    return this.todos;
   }
 
   addTodo(todo: Todo): void {
     this.todos.push(todo);
-    this.todosSubject.next(this.todos);
   }
 
   deleteTodo(id: number): void {
     this.todos = this.todos.filter(todo => todo.id !== id);
-    this.todosSubject.next(this.todos);
   }
-}
-
-interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
 }
