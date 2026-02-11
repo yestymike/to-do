2607df67b0697e0680e9aa484dfe19dc4fa2042a
Mike <27782678+yestymike@users.noreply.github.com>
2026-02-11 08:17:26 +0300

    Add TodoItem model

---
+++ backend/TodoApi/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
