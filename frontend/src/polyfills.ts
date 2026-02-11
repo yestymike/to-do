e4c2e9c38e056516454bb6b9aa7a17bceb66e929
Mike <27782678+yestymike@users.noreply.github.com>
2026-02-11 08:26:22 +0300

    Add polyfills.ts for frontend

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -4,25 +4,13 @@
  * 
  * This file is divided into 2 sections:
  *   1. Browser polyfills (run by browsers)
- *   2. Application imports (imported for use in Angular)
+ *   2. Application imports (optional, run by webpack)
  */
 
-/**************************************************************************************************
- * BROWSER POLYFILLS
- */
-
+// 1. Browser polyfills
 /**
  * By default, zone.js will patch all possible global variables.
- * For applications that are not using Angular or weaving Zone JS into Angular,
- * you can disable default behavior using {
-   provideZoneChangeDetection: { shouldRun: false }
- },
- * Then import and patch at a more granular level here.
+ * For applications that are not using Angular or weaving Zone JS with WebComponents then
+ * this can be safely removed.
  */
-
-// Import the zone.js polyfills required by Angular.
 import 'zone.js';
-
-/**************************************************************************************************
- * Zone JS is required by default for Angular itself.
- */
