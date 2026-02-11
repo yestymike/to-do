6707a81f7cd3a016fd20950f4260836a0f9d5f0a
Mike <27782678+yestymike@users.noreply.github.com>
2026-02-11 08:21:29 +0300

    Add app.component.spec.ts for frontend

---
+++ frontend/src/app/app.component.spec.ts
@@ -0,0 +1,20 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+  });
+
+  it('should create the app', () => {
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
